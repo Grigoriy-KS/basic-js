bde1c1ececa358dc24735b802f3bc04eeffb05f8
Grigoriy Ksyonz <KsyonzGS@gmail.com>
2025-01-07 02:14:43 +0300

    feat: add solution

--- src/what-season.js
+++ src/what-season.js
@@ -18,10 +18,33 @@ function getSeason(date) {
   if (!(date instanceof Date)) {
     throw new Error('Invalid date!');
   }
-  if (+date.toString().split(' ')[3] !== date.getFullYear()) {
-    throw new Error('Invalid date!');
-  }
-  if (date.hasOwnProperty('getFullYear')) {
+  const tempDate = new Date(date.toString());
+
+  const isDeepEqual = (object1, object2) => {
+
+    const objKeys1 = Object.keys(object1);
+    const objKeys2 = Object.keys(object2);
+  
+    if (objKeys1.length !== objKeys2.length) return false;
+  
+    for (let key of objKeys1) {
+      const value1 = object1[key];
+      const value2 = object2[key];
+  
+      const isObjects = isObject(value1) && isObject(value2);
+  
+      if ((isObjects && !isDeepEqual(value1, value2)) ||
+        (!isObjects && value1 !== value2)
+      ) {
+        return false;
+      }
+    }
+    return true;
+  };
+  const isObject = (object) => {
+    return object != null && typeof object === "object";
+  };
+  if (!isDeepEqual(date, tempDate)) {
     throw new Error('Invalid date!');
   }
   switch (date.getMonth()) {
